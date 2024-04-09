4cb321f19352561ff7c0ef20856a256619cde4c7
Przemek5901 <pbednarz10@gmail.com>
2024-04-09 12:39:54 +0200

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '**', 
+    component: SearchPageComponent
+  },
+  { 
+    path: '',   
+    redirectTo: 'search', 
+    pathMatch: 'full' },
+  {
+    path: 'search',
+    component: SearchPageComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfilePageComponent,
+  },
+];
